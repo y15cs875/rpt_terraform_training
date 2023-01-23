caa8028b551943cb5d3cabea69480fdbc765dca6
y15cs875 <45996053+y15cs875@users.noreply.github.com>
2023-01-23 21:02:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,40 @@ module "networking" {
   vnet_address_spacing    = var.vnet_address_spacing
   subnet_address_prefixes = var.subnet_address_prefixes
 }
+  
+
+module "webserver" {
+  source  = "app.terraform.io/lab_org_rpt/webserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[0]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "appserver" {
+  source  = "app.terraform.io/lab_org_rpt/appserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[1]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "dataserver" {
+  source  = "app.terraform.io/lab_org_rpt/dataserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[2]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
