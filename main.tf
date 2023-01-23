f6c65794dcbcf0560fadea0c7a34523afc144dd1
y15cs875 <45996053+y15cs875@users.noreply.github.com>
2023-01-23 19:51:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "name" {}
+variable "location" {}
+variable "username" {}
+variable "password" {}
+
+variable "vnet_address_spacing" {
+  type = list
+}
+
+variable "subnet_address_prefixes" {
+  type = list
+}
+
+module "networking" {
+  source  = "app.terraform.io/YOUR_ORG_NAME/networking/azurerm"
+  version = "0.12.0"
+
+  name                    = var.name
+  location                = var.location
+  vnet_address_spacing    = var.vnet_address_spacing
+  subnet_address_prefixes = var.subnet_address_prefixes
+}
